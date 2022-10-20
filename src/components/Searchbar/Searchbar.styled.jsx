cc4439d11104bfda9db6bf885739762320eec8cb
Reqvite <reqvite2233@gmail.com>
2022-10-20 17:51:00 +0300

    Update Searchbar.styled.jsx

--- src/components/Searchbar/Searchbar.styled.jsx
+++ src/components/Searchbar/Searchbar.styled.jsx
@@ -7,7 +7,6 @@ export const Header = styled.header`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 100%;
   min-height: 64px;
   padding-right: 24px;
   padding-left: 24px;
